30601aa762288742506c6caa2223b7a9ac430da3
hyd <hyd998877@163.com>
2022-03-03 00:14:27 +0800

    Add files via upload

--- variants/defaults-gpl.sh
+++ variants/defaults-gpl.sh
@@ -1,4 +1,4 @@
-FF_CONFIGURE="--enable-gpl --enable-version3 --disable-debug"
+FF_CONFIGURE="--enable-gpl --enable-version3 --disable-debug --disable-ffplay --disable-ffserver --enable-static --disable-shared --disable-encoders --enable-encoder=libmp3lame --enable-libmp3lame --enable-nonfree --disable-parsers --enable-demuxer=mp3 --disable-muxers --enable-muxer=mp3 --disable-decoders --enable-decoder=mp3"
 FF_CFLAGS=""
 FF_CXXFLAGS=""
 FF_LDFLAGS=""

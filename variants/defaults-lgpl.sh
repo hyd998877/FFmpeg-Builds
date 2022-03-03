c262eb33781c5f3f4ea2e1227db5e2a9926a6308
hyd <hyd998877@163.com>
2022-03-03 08:39:25 +0800

    Update defaults-lgpl.sh

--- variants/defaults-lgpl.sh
+++ variants/defaults-lgpl.sh
@@ -1,4 +1,4 @@
-FF_CONFIGURE="--enable-version3 --disable-debug  --disable-ffplay --disable-ffserver --enable-static --disable-shared --disable-encoders --enable-encoder=libmp3lame --enable-libmp3lame --enable-nonfree --disable-parsers --enable-demuxer=mp3 --disable-muxers --enable-muxer=mp3 --disable-decoders --enable-decoder=mp3"
+FF_CONFIGURE="--enable-version3 --disable-debug  --disable-ffplay --enable-static --disable-shared --disable-encoders --enable-encoder=libmp3lame --enable-libmp3lame --enable-nonfree --disable-parsers --enable-demuxer=mp3 --disable-muxers --enable-muxer=mp3 --disable-decoders --enable-decoder=mp3"
 FF_CFLAGS=""
 FF_CXXFLAGS=""
 FF_LDFLAGS=""
